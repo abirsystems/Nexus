6ba174012adcc2d7f691ce68b46d5cc39e9ab6d4
abirsystems <abircse63@gmail.com>
2026-09-19 10:37:21 +0530

    Update functions_check.py

--- functions_check.py
+++ functions_check.py
@@ -11,6 +11,7 @@ import pandas as pd
 import spacy
 import nltk
 nltk.download('punkt_tab')
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import sent_tokenize, word_tokenize
